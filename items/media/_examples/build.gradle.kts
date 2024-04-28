aa14dc8382f6902d9f551087f526457de5578e41
Jin Yang <u7724192@anu.edu.au>
2024-04-28 17:57:44 +1000

    Update the com.android.application plugin version in the project-level build.gradle file

--- items/media/_examples/build.gradle.kts
+++ items/media/_examples/build.gradle.kts
@@ -3,7 +3,6 @@ buildscript {
         classpath("com.google.gms:google-services:4.4.1")
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "7.4.2" apply false
 }
