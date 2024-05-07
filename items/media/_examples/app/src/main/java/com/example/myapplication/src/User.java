a95b5e9fe1e537b60387b8e02458fbbddbe9a1f1
Yingxuan Tang <u7670526@anu.edu.au>
2024-05-07 22:58:18 +1000

    Add 'userTpye' and 'posts' attributes in User class. Modify the way to generate userID. (UUID).

--- items/media/_examples/app/src/main/java/com/example/myapplication/src/User.java
+++ items/media/_examples/app/src/main/java/com/example/myapplication/src/User.java
@@ -3,51 +3,54 @@ package com.example.myapplication.src;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 这个类用于管理用户的信息。
- *
+ * <p>
  * 职责:
- *
+ * <p>
  * 存储用户信息，如用户名、邮箱、密码、联系信息等。
  * 可以包括用户的注册和登录方法，或者这部分功能由外部类处理。
  */
 
 public class User {
-    private static final AtomicInteger idGenerator = new AtomicInteger(0); // 从0开始
-    private static final Set<String> allocatedUserIds = new HashSet<>(); // 存储已分配的用户ID
     private String userId;
     private String email;  // Username 默认是
     private String passwordHash;
     private String name;
     private String address;
     private String phone;
-    private PostList postList; // 添加 PostList 属性
+    private PostList postList;  // 添加 PostList 属性
+    private String userType;    // "0":Admin    "1":normal user
+    private List<Post> posts;
+
 
     // 构造方法，只用邮箱和密码初始化
     public User(String email, String password) {
         // 因为Register只用邮箱和密码，所以用这两个信息初始化每个User实例
-        this.userId = generateUniqueId(); // 格式化为8位数字，包含前导零
+        this.userId = UUID.randomUUID().toString(); // 使用UUID生成唯一的用户ID
         this.email = email;
         this.passwordHash = password;//删掉了这里所有的hashPassword(password)方法，因为这里不需要哈希，否则会造成两次SHA256
+        this.userType = "1";
     }
-
 
     // 全参数构造方法,但是userId是自动生成的
     public User(String email, String password, String name, String address, String phone) {
-        this.userId = generateUniqueId();  // 生成唯一的用户ID
+        this.userId = UUID.randomUUID().toString();  // 使用UUID生成唯一的用户ID
         this.email = email;
         this.passwordHash = password;  // 对密码进行哈希处理
         this.name = name;
         this.address = address;
         this.phone = phone;
+        this.userType = "1";
     }
-    //这个构造方法是用来从firebase直接导入的，userId不用自动生成，已经分配过了
+
     public User(String userId, String email, String password, String name, String address, String phone) {
         this.userId = userId;  // 生成唯一的用户ID
-        allocatedUserIds.add(userId);
         this.email = email;
         this.passwordHash = password;  // 对密码进行哈希处理
         this.name = name;
@@ -55,14 +58,16 @@ public class User {
         this.phone = phone;
     }
 
-    // 生成一个唯一的ID，并确保它没有被之前分配过
-    private String generateUniqueId() {
-        String id;
-        do {
-            id = String.format("%08d", idGenerator.getAndIncrement());
-        } while (allocatedUserIds.contains(id));
-        allocatedUserIds.add(id);
-        return id;
+//这个构造方法是用来从firebase直接导入的，userId不用自动生成，已经分配过了，等firebase数据改好后，要调用这个来初始化User表
+    public User(String userId, String email, String password, String name, String address, String phone, String userType) {
+        this.userId = userId;  // 生成唯一的用户ID
+        this.email = email;
+        this.passwordHash = password;  // 对密码进行哈希处理
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.userType = userType;
+        this.posts = null;    // 先设为null，
     }
 
     // 使用SHA-256对密码进行哈希
