646ff470f8302e3650f2bede05022e137a63b9cf
Linxiang Hu <u7633783@anu.edu.au>
2024-05-08 09:45:05 +1000

    Finish the operation that adding posts to firebase

--- items/media/_examples/app/src/main/java/com/example/myapplication/src/Firebase/PostManager/FirebasePostHelper.java
+++ items/media/_examples/app/src/main/java/com/example/myapplication/src/Firebase/PostManager/FirebasePostHelper.java
@@ -1,49 +1,50 @@
 package com.example.myapplication.src.Firebase.PostManager;
 
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.myapplication.src.Post;
-import com.example.myapplication.src.Tag;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class FirebasePostHelper {
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference myRef = database.getReference("Post");
+
+    public void addPost(Post post) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference().child("post");
 
 
-//    private void addPost(Post post) {
-//
-//        DatabaseReference newPostRef = myRef.push();
-//
-//        newPostRef.child("userID").setValue(post.getPostID());
-//        newPostRef.child("postID").setValue(post.getPostID());
-//        newPostRef.child("gender").setValue(post.getTag().getGender());
-//        newPostRef.child("masterCategory").setValue(post.getTag().getMasterCategory());
-//        String subCategory = newPostRef.child("subCategory").setValue(post.getTag().);
-//        String articleType = snapshot.child("articleType").getValue(String.class);
-//        String baseColour = snapshot.child("baseColour").getValue(String.class);
-//        String season = snapshot.child("season").getValue(String.class);
-//        String year = snapshot.child("year").getValue(String.class);
-//        String usage = snapshot.child("usage").getValue(String.class);
-//        String productDisplayName = snapshot.child("productDisplayName").getValue(String.class);
-//        String price = snapshot.child("price").getValue(String.class);
-//        String status = snapshot.child("status").getValue(String.class);
-//        String imageUrl = snapshot.child("image_url").getValue(String.class);
-//        String description = snapshot.child("description").getValue(String.class);
-//        String comments = snapshot.child("comment").getValue(String.class);
-//
-//        assert year != null;
-//        assert price != null;
-//
-//        // 创建Tag对象
-//        Tag.ArticleType articleTypeObj = new Tag.ArticleType(articleType);
-//        Tag.SubCategory subCategoryObj = new Tag.SubCategory(subCategory, articleTypeObj);
-//        Tag.MasterCategory masterCategoryObj = new Tag.MasterCategory(masterCategory, subCategoryObj);
-//        Tag tag = new Tag(gender, masterCategoryObj, baseColour, season, Integer.parseInt(year), usage);
-//
-//
-//    }
+        Log.d("Firebase add operation", "Enter the method");
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("Firebase add operation", "Execute the method");
+                // 这将给出"user"下子节点的数量
+                long count = dataSnapshot.getChildrenCount();
+                // 现在在这个新索引下设置新用户数据
+                DatabaseReference newPostRef = myRef.child(String.valueOf(count));
+                newPostRef.child("postID").setValue(post.getPostID());
+                newPostRef.child("userID").setValue(post.getUserID());
+                newPostRef.child("productDisplayName").setValue(post.getProductDisplayName());
+                newPostRef.child("price").setValue(Double.toString(post.getPrice()));
+                newPostRef.child("status").setValue(post.getStatus());
+                newPostRef.child("imageUrl").setValue(post.getImageUrl());
+                newPostRef.child("description").setValue(post.getDescription());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("Firebase add operation failed", "Failure to add post to firebase：" + databaseError.getCode());
+            }
+        });
+    }
 
 
 }
