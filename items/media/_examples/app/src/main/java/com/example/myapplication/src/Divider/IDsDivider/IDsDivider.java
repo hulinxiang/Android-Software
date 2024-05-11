37c1123e0684f26a301cf7c98760cd1a739a3de9
Yichi Zhang <u7748799@anu.edu.au>
2024-05-11 22:06:40 +1000

    Add divider template method pattern

---
+++ items/media/_examples/app/src/main/java/com/example/myapplication/src/Divider/IDsDivider/IDsDivider.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.src.Divider.IDsDivider;
+
+import com.example.myapplication.src.Divider.Divider;
+
+public class IDsDivider extends Divider<String> {
+    @Override
+    protected String getDivider() {
+        return ",";
+    }
+}
