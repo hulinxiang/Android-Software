0b7977b156988870ee90317a8fb4908699bcd344
Linxiang Hu <u7633783@anu.edu.au>
2024-05-15 23:27:03 +1000

    Use thread to finish data stream that updates the home page automatically.

--- items/media/_examples/app/src/main/java/com/example/myapplication/activity/HomeActivity.java
+++ items/media/_examples/app/src/main/java/com/example/myapplication/activity/HomeActivity.java
@@ -3,8 +3,9 @@ package com.example.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,22 +17,11 @@ import android.widget.TextView;
 import com.example.myapplication.BPlusTree.Post.BPlusTreeManagerPost;
 import com.example.myapplication.R;
 import com.example.myapplication.src.Post;
-import com.example.myapplication.src.PostList;
-import com.example.myapplication.src.User;
 
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Context;
-import android.net.Uri;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 import com.example.myapplication.activity.Image.GlideImageLoader;
+
 public class HomeActivity extends AppCompatActivity {
 
     private LinearLayout home;
@@ -42,8 +32,8 @@ public class HomeActivity extends AppCompatActivity {
 
     private GridLayout gl_post;
 
-
-
+    private Handler handler = new Handler();
+    private Runnable runnable;
 
 
     @Override
@@ -55,7 +45,7 @@ public class HomeActivity extends AppCompatActivity {
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,HomeActivity.class);
+                Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
@@ -71,7 +61,7 @@ public class HomeActivity extends AppCompatActivity {
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,CreateActivity.class);
+                Intent intent = new Intent(HomeActivity.this, CreateActivity.class);
                 startActivity(intent);
             }
         });
@@ -79,7 +69,7 @@ public class HomeActivity extends AppCompatActivity {
         inbox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,InboxActivity.class);
+                Intent intent = new Intent(HomeActivity.this, InboxActivity.class);
                 startActivity(intent);
             }
         });
@@ -87,7 +77,7 @@ public class HomeActivity extends AppCompatActivity {
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,ProfileActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
                 startActivity(intent);
             }
         });
@@ -98,35 +88,38 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    private void showPost(){
+    private void showPost() {
+        Log.d("Thread========", "showPost started");
+
+        gl_post.removeAllViews();
         //需要一个list
         List<Post> list = BPlusTreeManagerPost.randomRecommender(getApplicationContext());
 
-        for (Post post: list){
+        for (Post post : list) {
             //get the layout from item_card.xml
-            View view = LayoutInflater.from(this).inflate(R.layout.item_card,null);
+            View view = LayoutInflater.from(this).inflate(R.layout.item_card, null);
             ImageView card_image = view.findViewById(R.id.card_image);
             TextView card_name = view.findViewById(R.id.card_name);
             TextView card_price = view.findViewById(R.id.card_price);
 
-            GlideImageLoader.loadImage(HomeActivity.this,post.getImageUrl(),card_image);
+            GlideImageLoader.loadImage(HomeActivity.this, post.getImageUrl(), card_image);
             card_name.setText(post.getProductDisplayName());
             card_price.setText(String.valueOf(post.getPrice()));
 
             //get the height and weight from the screen
             int screenWidth = getResources().getDisplayMetrics().widthPixels;
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(screenWidth/2, ViewGroup.LayoutParams.WRAP_CONTENT);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(screenWidth / 2, ViewGroup.LayoutParams.WRAP_CONTENT);
             //add to grid layout
-            gl_post.addView(view,params);
+            gl_post.addView(view, params);
 
             //click image jump to post detail page
-            card_image.setOnClickListener(v ->{
-                Intent intent = new Intent(HomeActivity.this,PostActivity.class);
-                intent.putExtra("post_id",post.getPostID());
-                intent.putExtra("post_image",post.getImageUrl());
-                intent.putExtra("post_name",post.getProductDisplayName());
-                intent.putExtra("post_description",post.getDescription());
-                intent.putExtra("post_price",post.getPrice());
+            card_image.setOnClickListener(v -> {
+                Intent intent = new Intent(HomeActivity.this, PostActivity.class);
+                intent.putExtra("post_id", post.getPostID());
+                intent.putExtra("post_image", post.getImageUrl());
+                intent.putExtra("post_name", post.getProductDisplayName());
+                intent.putExtra("post_description", post.getDescription());
+                intent.putExtra("post_price", post.getPrice());
                 intent.putExtra("post_seller", post.getUserID());
                 intent.putExtra("source", "home");
                 startActivity(intent);
@@ -135,12 +128,44 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-    private void init(){
+    private void init() {
         home = findViewById(R.id.btn_home);
         search = findViewById(R.id.btn_search);
         create = findViewById(R.id.btn_create);
         inbox = findViewById(R.id.btn_inbox);
         profile = findViewById(R.id.btn_profile);
+    }
+
+    private void startRepeatingTask() {
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(() -> {
+                    Log.d("Thread====", "Running on UI thread");
+                    showPost();
+                });
+                handler.postDelayed(this, 10000);
+            }
+        };
+        handler.post(runnable);
+    }
+
+    private void stopRepeatingTask() {
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopRepeatingTask(); // 停止重复的任务
+        Log.d("Thread===", "我消灭了线程");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startRepeatingTask(); // 在Activity开始时重新启动任务
+        Log.d("Thread===", "我启动了线程");
     }
 
 }
